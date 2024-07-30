e118288f0347c84c9fd2b64289e6f7d814b36ab0
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-07-30 10:40:26 +0530

    counter

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react' // TODO hook import here useState is imported u can import more like import { useState  , useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {

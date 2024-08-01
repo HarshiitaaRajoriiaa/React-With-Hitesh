29f7b4248e3ad140255065e12fbc76979cab3ff5
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-08-01 15:15:57 +0530

    c

--- Hooks/UseEffect/FetchingApi/Fetch.jsx
+++ Hooks/UseEffect/FetchingApi/Fetch.jsx
@@ -1,7 +1,46 @@
-import { useState , useEffect } from "react";
-import axios from 'react';
+import React, { useEffect , useState } from "react";
 import './Fetch.css';
+function FetchingApi(){
+    const [data , setData] = useState(null)
+    const [loading , setloading] = useState(null)
 
-function fetch(){
-    
+
+    // ? take a function
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(Response =>{
+            Response.json()
+            setloading(false) // TODO after fetching data from API set Loading false == loading finish
+        }).catch(error=>{
+            console.log(`error while fetching API ${error}`)
+            alert(`${error}`)
+            setloading(false) // TODO if error occurred stop Loading and show alert message
+        })
+    })
+
+    if(loading){
+        <div>loading...</div>
+    }
+
+
+
+    return (
+        <>
+        <h1>Fetching Data from API </h1>
+        <a>https://jsonplaceholder.typicode.com/posts</a>
+        <div>
+            <h3>Data after fetching</h3>
+            {/* <ul>
+            {data.map(post => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+            </ul> */}
+        </div>
+        </>
+    )
+
 }
+export default FetchingApi
---
+++ Hooks/UseEffect/Notes.md
@@ -0,0 +1,6 @@
+### useEffect():
+* The useEffect Hook allows you to perform side effects in your components.
+* useEffect accepts two arguments. The second argument is optional.
+``` js
+ useEffect(<function>, <dependency>)
+```

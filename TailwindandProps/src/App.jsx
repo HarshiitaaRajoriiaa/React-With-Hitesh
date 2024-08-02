9a6b2b3cf2b27291c62c24d0eaaab300b0184e25
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-08-02 12:17:36 +0530

    commit

--- TailwindandProps/src/App.jsx
+++ TailwindandProps/src/App.jsx
@@ -8,26 +8,68 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* text01 */}
+      <h1 className='font-extralight font-size'>Tailwind Test</h1>
+
+      {/* card 01-mountains */}
+      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800 h-96 w-96">
+        <img className="w-24 h-24 rounded-full" src="https://images.pexels.com/photos/618833/pexels-photo-618833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width="384" height="512" />
+        <div className="pt-6 space-y-4">
+          <blockquote>
+            <p className="text-lg">
+              “Tailwind CSS is the only framework that I've seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption>
+            <div>
+              Sarah Dayan
+            </div>
+            <div>
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
+        </div>
+      </figure>
+
+
+      {/* card 02 - girl */}
+      <div class="relative h-[400px] w-[300px] rounded-md m-12">
+        <img
+          src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+          alt="AirMax Pro"
+          class="z-0 h-full w-full rounded-md object-cover"
+        />
+        <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+        <div class="absolute bottom-4 left-4 text-left">
+          <h1 class="text-lg font-semibold text-white">Delba</h1>
+          <p class="mt-2 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+            debitis?
+          </p>
+          <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+            View Profile →
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {/*  card-03 - rotation. */}
+      {/* <div className="relative w-72 h-96 card-container">
+        <div className="absolute inset-0 card">
+          <div className="absolute inset-0 card-front bg-cover bg-center rounded-md" style="background-image: url('https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60');">
+            <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent rounded-md"></div>
+            <div className="absolute bottom-4 left-4 text-left">
+              <h1 className="text-lg font-semibold text-white">Delba</h1>
+            </div>
+          </div>
+          <div className="absolute inset-0 card-back bg-gray-900 flex items-center justify-center rounded-md">
+            <p className="text-sm text-gray-300 px-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
+            </p>
+          </div>
+        </div>
+      </div> */}
     </>
   )
 }

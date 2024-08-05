560fd2614945573ae6c48f494a48ad8d9fa6b214
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-08-05 14:57:13 +0530

    React

---
+++ BgChanger/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="font-mono">BG Changer</h1>
+    </>
+  )
+}
+
+export default App
---
+++ Counter/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react' //import hooks from react
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0); //return array
+  const minusValue = () => {
+    console.log("minus clicked")
+    setCounter(counter - 1);
+  }
+  return (
+    <>
+      <div class="main">
+        <h1 class="text">Counter:</h1>
+        <div class="rect"></div>
+        <div class="display">{counter}</div>
+        <button class="add" onClick={() => {
+          console.log("add clicked")
+          setCounter(counter + 1); // counter = 1
+          // ? what will happen??
+          setCounter(counter + 1); // counter = 2 or not
+          setCounter(counter + 1); // counter = 3 or not
+          setCounter(counter + 1); // counter = 4 or not
+          // ! in react useState send all the changes to UI in batches like setCounter(counter + 1); in every line doing same that's why it is considering it as same in all the lines. 
+          // that's why value incremented to 1 only irrespective of how many times you have been call the method setCounter()
+
+
+          setCounter((previousValue) => previousValue + 1);
+          setCounter((previous) => previous + 1);
+          setCounter((Counter) => Counter + 1);
+          setCounter((previousValue) => {
+            return previousValue + 1;
+          });
+          // now this will update value by 1 every time you call it. previousValue is a variable can have any other name too.
+          //* as setCounter returns a callback function and previous value of whatever you put it on.
+        }} disabled={counter >= 20}>+</button>
+        <button class="minus" onClick={minusValue} disabled={counter < 1}>-</button>
+      </div>
+    </>
+  )
+}
+export default App

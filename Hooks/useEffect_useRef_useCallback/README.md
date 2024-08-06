e99837b1b04ee5dffed4fc08956e527ccdad4196
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-08-06 14:30:21 +0530

    Password

---
+++ Hooks/useEffect_useRef_useCallback/README.md
@@ -0,0 +1,7 @@
+## PASSWORD GENERATOR :
+* using useCallback( ) , useEffect( ), useRef( )
+### useCallback( ):
+* Imagine you have a toy box, and you keep getting the same toy out every time you play. You don't want to search for it again and again. Instead, you remember exactly where you put it last time so you can get it quickly next time. This saves time and effort 
+* React does the same , it remembers the function so that it doesn't create it from scratch every time your component updates. This can make your project faster.
+* useCallback( ) stores  frequent using functions in cache memory for future use.
+### useEffect()
---
+++ Hooks/useEffect_useRef_useCallback/src/App.jsx
@@ -0,0 +1,89 @@
+import { useState , useCallback , useEffect} from 'react'
+// import './App.css'
+
+function App() {
+
+
+  const [length , setLength] = useState(8) //* initial length will be 8
+  const [number , numberAllowed]= useState(false)
+  const [char, specialCharAllowed]= useState(false)
+  const [password , setPassword] = useState("") //*initially nothing
+
+//TODO generate random password using alphanumeric characters corresponding to the conditions. [ number allowed? , special character allowed?]
+  const generator = useCallback(()=>{
+    let pass = " ";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numberAllowed) str+= "0123456789"
+    if(specialCharAllowed) str+= "!@#$%^&*(){}:?|[]"
+    for (let i  = 1; i  <= length; i ++) {
+      let index = Math.floor((Math.random()*str.length)+1) 
+        pass +=str.charAt(index);     
+    }
+    setPassword(pass);
+
+  } , [length, number,char, setPassword ])
+  useEffect(()=>{
+      generator()
+  } , [length , numberAllowed , specialCharAllowed])
+  const copy = ()=>{
+
+  }
+
+  return (
+    <>
+    <h1 className='text-sky-700 text-center text-4xl m-36 mb-10 items-center font-thin flex justify-center'><i>Password Generator:</i></h1>
+    {/* <div className='height-[200] bg-sky-700 width-[600] ml-12 items-end justify-center rounded-2xl'></div> */}
+    <div className='bg-slate-800 text-sky-700 rounded-2xl w-full h-auto p-10 mx-auto my-auto shadow-2xl '>
+      <div className= 'bg-white text-blue-950 rounded-2xl w-full h-10 overflow-hidden mb-4 flex'>
+        <input 
+        className='outline-none w-full py-1 px-3'
+        type ="text"
+        value ={password}
+        placeholder="password"
+        readOnly
+        />
+        <button 
+        className='outline-none bg-lime-700 text-white px-3 py-0.5 shrink-1 rounded-lg ml-7'>Copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input 
+          type="range"
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer '
+          onChange={(e)=>{
+            setLength(e.target.value)
+          }}
+          />
+          <label className='mr-2'>Length : {length}</label>
+          <input 
+          type="checkbox"
+          defaultChecked = {numberAllowed}
+          onChange={()=>{
+            numberAllowed((previous)=>{
+              !previous;
+            }) 
+          }}  
+          />
+          <label className='mr-2'>Special character?</label>
+          <input 
+          type="checkbox"
+          defaultChecked = {specialCharAllowed}
+          onChange={()=>{
+            specialCharAllowed((previous)=>{
+              !previous;
+            }) 
+          }}  
+          />
+          <label className='mr-2'> Numbers? </label>
+        </div>
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default App

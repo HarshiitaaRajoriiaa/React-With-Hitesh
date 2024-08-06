1ad5dbb2296352752bd3fbc78a50b5b8928b9361
Harshita Rajoria <harshitarajoria02@gmail.com>
2024-08-06 16:54:09 +0530

    commit

--- Hooks/CustomHooks/src/Custom Hook/useCurrInfo.js
+++ Hooks/CustomHooks/src/Custom Hook/useCurrInfo.js
@@ -4,9 +4,11 @@ function useCurrencyInfo(currency){
     useEffect(()=>{
 // *https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/usd.json
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
-    } , [])
+    } , [currency])
     .then((data)=> data.json())
-    .then()
-    .catch()
-    return [];
+    .then((data)=> setCurr(data[currency]))
+    return data;
+
+   
 }
+export default useCurrencyInfo; 
